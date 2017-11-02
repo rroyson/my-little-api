2e3eaf06992ed941b0550ab59a2155707995852a
rroyson <frontiersons@gmail.com>
2017-11-01 23:20:58 -0400

    routed GET endpoints, modified README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # my-little-api
+
+## Introduction
+
+## Getting Started
+
+Welcome to the internal API team. As a developer on the team (not a public developer) use the instructions below to clone the repo, install dependencies, and start the API.
+
+```
+$ git clone https://github.com/rroyson/my-little-api.git
+$ npm install
+$ npm start
+```
---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const cats = [
+  {
+    id: 'tabby',
+    type: 'breed',
+    desc: 'tabbblees'
+  },
+  {
+    id: 'siamese',
+    type: 'breed',
+    desc: 'crazy'
+  },
+  {
+    id: 'maine coon',
+    type: 'breed',
+    desc: 'lol'
+  },
+  {
+    id: 'minx',
+    type: 'breed',
+    desc: 'sexy'
+  },
+  {
+    id: 'felix',
+    type: 'cat',
+    name: 'felix',
+    breed: 'tabby',
+    owner: 'John Jones',
+    age: 10
+  },
+  {
+    id: 'don',
+    type: 'cat',
+    name: 'don',
+    breed: 'tabby',
+    owner: 'Lucas Sams',
+    age: 7
+  },
+  {
+    id: 'fuzz',
+    type: 'cat',
+    name: 'fuzz',
+    breed: 'maine coon',
+    owner: 'kat bird',
+    age: 4
+  },
+  {
+    id: 'fat',
+    type: 'cat',
+    name: 'fat',
+    breed: 'minx',
+    owner: 'rob royson',
+    age: 10
+  }
+]
+
+app.get('/', function(req, res) {
+  // console.log('req', req)
+  // console.log('res', res)
+  res.send('Welcome to the cat api')
+})
+
+app.get('/breeds', function(req, res) {
+  // console.log('req', req)
+  // console.log('res', res)
+  res.send('/breeds works')
+})
+
+app.get('/breeds/:id', function(req, res) {
+  const breedID = req.params.id
+  res.send(breedID)
+})
+
+app.get('/cats', function(req, res) {
+  res.send('/cats works')
+})
+
+app.get('/cats/:id', function(req, res) {
+  const catID = req.params.id
+  res.send(catID)
+})
+
+app.listen(port, () => console.log('API is up one port', port))
